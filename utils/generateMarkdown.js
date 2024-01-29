e16fdfca33e47801ae1f7b2d04ae1eb7599f402d
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-01-29 19:47:00 +0000

    I created switch statements for the LicenseSection and the LicenseLink.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // I chose to create a switch statement instead of an if/else if because it looks cleaner and easier to read.
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-var licenseBadge = "";
+let licenseBadge = "";
 switch (license === "MIT") {
   case "MIT":
   licenseBadge = "";
@@ -28,11 +28,59 @@ return licenseBadge;
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+switch (license === "MIT") {
+  case "MIT":
+    licenseLink = "";
+  break;}
+switch (license === "APACHE 2.0") {
+  case "APACHE 2.0":
+    licenseLink = "";
+  break;}
+switch (license === "GPL 3.0") {
+  case "GPL 3.0":
+    licenseLink = "";
+  break;}
+switch (license === "BSD 3") {
+  case "BSD 3":
+    licenseLink = "";
+  break;}
+switch (license === "None") {
+  case "None":
+    licenseLink = "";
+  break;}
+return licenseLink;
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+switch (license === "MIT") {
+  case "MIT":
+    licenseSection = "";
+  break;}
+switch (license === "APACHE 2.0") {
+  case "APACHE 2.0":
+    licenseSection = "";
+  break;}
+switch (license === "GPL 3.0") {
+  case "GPL 3.0":
+    licenseSection = "";
+  break;}
+switch (license === "BSD 3") {
+  case "BSD 3":
+    licenseSection = "";
+  break;}
+switch (license === "None") {
+  case "None":
+    licenseSection = "";
+  break;}
+return licenseLink;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
