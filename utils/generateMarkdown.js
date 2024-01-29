10c8b87dfcdc28569984e0e6e0f6869264b9a2e7
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-01-29 19:43:46 +0000

    Created a switch statement for the renderLicenseBadge function.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// I chose to create a switch statement instead of an if/else if because it looks cleaner and easier to read.
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+var licenseBadge = "";
+switch (license === "MIT") {
+  case "MIT":
+  licenseBadge = "";
+  break;}
+switch (license === "APACHE 2.0") {
+  case "APACHE 2.0":
+  licenseBadge = "";
+  break;}
+switch (license === "GPL 3.0") {
+  case "GPL 3.0":
+  licenseBadge = "";
+  break;}
+switch (license === "BSD 3") {
+  case "BSD 3":
+  licenseBadge = "";
+  break;}
+switch (license === "None") {
+  case "None":
+  licenseBadge = "";
+  break;}
+return licenseBadge;
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
