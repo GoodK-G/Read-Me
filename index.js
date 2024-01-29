d049e89a6eb64a8f5eb0d32e15cd712c7fa30791
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-01-29 20:45:09 +0000

    Created the questions array and made some changes to the writeToFile function. Also, created the function to init the app.

--- index.js
+++ index.js
@@ -2,19 +2,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = ['What license are you using to create this README file?'];
+const questions = [
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license are you using to create this README file?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const fileName = 'README.md'
-fs.writeFile(fileName, JSON.stringify(data), (err) =>
+fs.writeFile(fileName, JSON.stringify(data, null, 2), (err) =>
 err ? console.log(err) : console.log('Successfully wrote the information to the file!')
 );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-}
+    inquirer.prompt(questions)
+    .then((answers)=> writeToFile('README.md', answers))
+    .then(()=> console.log('README.md has been written to!'))
+    .catch((err)=>console.error(err));
+};
+    
+
 
 // Function call to initialize app
 init();
